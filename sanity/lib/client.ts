ada83d3f55079167c22e09f5c2efe14157ed6e37
Felice Forgione <forgione67@gmail.com>
2024-05-09 15:34:38 -0400

    updated client to conditional query published content

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,10 +1,12 @@
 import { createClient } from "next-sanity";
 
-import { apiVersion, dataset, projectId, useCdn } from "../env";
+import { apiVersion, dataset, projectId, useCdn, apiKey } from "../env";
 
 export const client = createClient({
   apiVersion,
   dataset,
   projectId,
   useCdn,
+  token: apiKey,
+  perspective: process.env.NODE_ENV === "development" ? "raw" : "published",
 });
